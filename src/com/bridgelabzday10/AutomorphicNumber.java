311c128d14e8aa9942cd5f12673eb1180a234b8d
Ameet Jadhav <ammugs11@gmail.com>
2022-09-05 16:36:55 +0530

    added Automorphic Number

---
+++ src/com/bridgelabzday10/AutomorphicNumber.java
@@ -0,0 +1,31 @@
+package com.bridgelabzday10;
+
+public class AutomorphicNumber {
+	
+	public static void main(String[] args) {
+		   
+		int n = 625, sq = n * n ;
+	    if(isAutomorphic(n) == 1)
+	        System.out.println("Num: "+ n + ", Square: " + sq + " - is Automorphic");
+	    else
+	        System.out.println("Num: "+ n + ", Square: " + sq + " - is not Automorphic");
+		   	
+		}
+		
+		static int isAutomorphic(int n){
+	    int square = n * n;
+	    while(n != 0)
+	    {
+	        
+	        if(n % 10 != square % 10){
+	            return 0;
+	        }
+	        
+	        n /= 10;
+	        square /= 10;
+	    }
+	   
+	    return 1;
+	}
+
+}
