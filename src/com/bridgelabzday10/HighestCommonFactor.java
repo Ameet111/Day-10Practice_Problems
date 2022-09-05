4b8d268715addbebfe5cf758b436436dd83d91de
Ameet Jadhav <ammugs11@gmail.com>
2022-09-05 17:10:33 +0530

    added Highest Common Factor

---
+++ src/com/bridgelabzday10/HighestCommonFactor.java
@@ -0,0 +1,18 @@
+package com.bridgelabzday10;
+
+public class HighestCommonFactor {
+	
+	 public static void main (String[]args) {
+	    int num1 = 12, num2 = 18, hcf=0;
+
+	    for (int i = 1; i <= num1 || i <= num2; i++)
+	      {
+	     if (num1 % i == 0 && num2 % i == 0)
+	        hcf = i;
+	      }
+
+	    System.out.println("The HCF: "+ hcf);
+	    
+	  }
+
+}
